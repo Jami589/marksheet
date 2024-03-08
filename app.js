fa1dfa8bb0fa25be6c59e5f69dd1ebc519284bd6
Jami589 <154450574+Jami589@users.noreply.github.com>
2024-03-09 02:23:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+let math = +prompt ("type your math marks")
+
+let english = +prompt ("type your math marks")
+
+let urdu = +prompt ("type your math marks")
+
+let chemistry = +prompt ("type your math marks")
+
+let plus = (math + english + urdu + chemistry ) /400 *100
+
+document.write ("Your Percentage is ",  plus + " %")
+
+
+if(plus > 50){
+    document.write ("pass")
+
+}
+
+else {
+    document.write ("fail")
+}
+
+
